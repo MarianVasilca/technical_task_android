e442583828278f4e3f3cb0e994bc81358e43ffdd
Marian Vasilca <marian.vasilca@gmail.com>
2022-01-04 20:02:35 +0200

    - Initial setup

---
+++ app/src/main/java/com/sliide/app/UsersApplication.kt
@@ -0,0 +1,4 @@
+package com.sliide.app
+
+class UsersApplication {
+}
---
+++ app/src/main/java/com/sliide/app/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sliide.app.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sliide.app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
